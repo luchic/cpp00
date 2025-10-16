dcfc863dcc75139389d06ed8147c19bd25edf5de
Nikita Luchinin <nluchini@student.42heilbronn.de>
2025-10-16 21:25:31 +0200

    Add ex02

---
+++ ex02/src/Account.cpp
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 18:09:21 by nluchini          #+#    #+#             */
+/*   Updated: 2025/10/16 21:23:02 by nluchini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) :
+	_amount(initial_deposit),
+	_nbDeposits(0),
+	_nbWithdrawals(0)
+{
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+}
+
+Account::~Account(void) {}
+
+int	Account::getNbAccounts(void)
+{
+	return _nbAccounts;
+}
+
+int	Account::getTotalAmount(void)
+{
+	return _totalAmount;
+}
+
+int	Account::getNbDeposits(void)
+{
+	return _totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return _totalNbWithdrawals;
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() 
+			  << "; total:" << getTotalAmount()
+			  << "; deposits:" << getNbDeposits()
+			  << "; withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	_amount += deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+	_totalAmount += deposit;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	if (withdrawal > _amount) {
+		return false;
+	}
+	_amount -= withdrawal;
+	_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	_totalAmount -= withdrawal;
+	return true;
+}
+
+int		Account::checkAmount(void) const
+{
+	return _amount;
+}
+
+void	Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << "index: " << _accountIndex
+			  << "; amount: " << _amount
+			  << "; deposits: " << _nbDeposits
+			  << "; withdrawals: " << _nbWithdrawals << std::endl;
+}
+
+void	Account::_displayTimestamp(void)
+{
+	std::time_t	time = std::time(0);
+	std::tm		*ltm = std::localtime(&time);
+	
+	std::cout << "[" << 1900 + ltm->tm_year
+			  << std::setfill('0') << std::setw(2) << 1 + ltm->tm_mon
+			  << std::setfill('0') << std::setw(2) << ltm->tm_mday << "_"
+			  << std::setfill('0') << std::setw(2) << ltm->tm_hour
+			  << std::setfill('0') << std::setw(2) << ltm->tm_min
+			  << std::setfill('0') << std::setw(2) << ltm->tm_sec << "] ";
+}
